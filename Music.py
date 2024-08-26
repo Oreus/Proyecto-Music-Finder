10f2b39518465445454c7678215cdc678cd758a2
Oscar Lopez Cardoso <oreus14@outlook.com>
2024-08-25 20:40:13 -0600

    Added inital python code and data base

---
+++ Music.py
@@ -0,0 +1,16 @@
+songs_db = [
+    {"title": "Blinding Lights", "artist": "The Weeknd", "genre": "Synthwave", "length": 200, "release_year": 2019},
+    {"title": "Levitating", "artist": "Dua Lipa", "genre": "Pop", "length": 203, "release_year": 2020},
+    {"title": "Save Your Tears", "artist": "The Weeknd", "genre": "Synthpop", "length": 215, "release_year": 2020},
+    {"title": "Don't Start Now", "artist": "Dua Lipa", "genre": "Pop", "length": 183, "release_year": 2019},
+    {"title": "Good 4 U", "artist": "Olivia Rodrigo", "genre": "Pop Rock", "length": 178, "release_year": 2021},
+    {"title": "Peaches", "artist": "Justin Bieber", "genre": "R&B", "length": 198, "release_year": 2021},
+    {"title": "Watermelon Sugar", "artist": "Harry Styles", "genre": "Pop", "length": 173, "release_year": 2019},
+    {"title": "Montero (Call Me By Your Name)", "artist": "Lil Nas X", "genre": "Hip-Hop", "length": 137, "release_year": 2021},
+    {"title": "Dynamite", "artist": "BTS", "genre": "K-Pop", "length": 199, "release_year": 2020},
+    {"title": "Stay", "artist": "The Kid LAROI", "genre": "Pop", "length": 141, "release_year": 2021}
+]
+
+song_count = 0
+
+print("total song count:", song_count + len(songs_db))
